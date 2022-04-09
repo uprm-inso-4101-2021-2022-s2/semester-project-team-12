52eafab833cb8fcda1e9b23f9c057c9ff9e0aa1b
moisesrobles04 <moises.robles@upr.edu>
2022-04-09 18:00:52 -0400

    Test DB

---
+++ ares/Procfile
@@ -0,0 +1,1 @@
+web: node ./components/App.js
---
+++ ares/src/Backend/database.js
@@ -0,0 +1,12 @@
+// import firebase from 'firebase'
+//
+//
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBLZhqpS6v0tGGQvYyvOQXtUBOsRYbyDTw",
+//     authDomain: "arres-team12.firebaseapp.com",
+//     projectId: "arres-team12",
+//     storageBucket: "arres-team12.appspot.com",
+//     messagingSenderId: "22388343174",
+//     appId: "1:22388343174:web:3269d884e187c9f9eaec07",
+//     measurementId: "G-73YZ6HGQT5"
+// };
