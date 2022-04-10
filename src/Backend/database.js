8748dd879679ea6094c6a4e8c532012b334826ba
moisesrobles04 <moises.robles@upr.edu>
2022-04-10 00:53:04 -0400

    FINALLY DE DATABASE IS CONNECTED AND WORKING

--- src/Backend/database.js
+++ src/Backend/database.js
@@ -2,15 +2,19 @@ const {Client} = require('pg');
 
 const client = new Client ({
     host: "ec2-18-214-134-226.compute-1.amazonaws.com",
-    port: 5432,
+    port:5432,
     user: "lnnbqdnuqfxyts",
     password: "c9f40136a6003f07d7c62a14cf4dc50f242c53e03b6b44733f717468085e81a7",
     database: "d24aijjaddl6n2",
+
+    ssl:{
+        rejectUnauthorized: false
+    }
 })
 
 client.connect();
 
-client.query('select * from User', (err, res)=>{
+client.query('SELECT * FROM "User"', (err, res)=>{
     if (!err){
         console.log(res.rows);
     }
